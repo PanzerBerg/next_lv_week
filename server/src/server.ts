d959c2bc1215f7928be32b34212bff9c03a2cb09
Matheus Santos <matheus.san99@gmail.com>
2020-06-02 23:21:39 -0300

    aula 2 concluida

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,29 @@
 import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+import path from 'path';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('listagem de usuários');
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-
-
-    response.json([
-        'Diego',
-        'José',
-        'Batata',
-        'Jantinha'
-    ]);
-});
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
+
+//Rota: Endereço completo da requisição
+//Recurso: Qual entidade estamos acessando do sistema
+
+//GET: Buscar uma ou mais informações do back-end
+//POST: Criar uma nova informação no bakc-end
+//PUT: Atualizar uma informação existente no back-end
+//DELETE: Remover uma informação doback-end
+
+//Request Param: Parametros que vem na própria rota que identificam um recurso
+//Query Param: Parâmetros que vem na própria rota geralmente opcionais que filtros
+//Request Body: Parametros para criação/atualização de informações
+
+//SELECT * FROM users WHERE name = 'Diego'
+//knex('users').where('name', 'Diego').select('*')
